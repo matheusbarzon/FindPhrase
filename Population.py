025a1b51e11b1fe23ec34890368e4d966d3269a6
Matheus <matheus.barzon@gmail.com>
2021-12-25 18:35:39 -0300

    Criacao do Main Criacao do Generation Criacao do Populacao de teste para desenvolvimento

--- Population.py
+++ Population.py
@@ -1,3 +1,5 @@
+import string
+
 from Chromosome import Chromosome
 
 class Population:
@@ -17,13 +19,45 @@ class Population:
 
       if i == 0:
         vChromosome.createDefineChromosome(['T','e','s','t','e'])
+        self.chromosome.append(vChromosome)
+        vChromosome = Chromosome(self)
+        vChromosome.createDefineChromosome(['T','e','s','t','x'])
+        self.chromosome.append(vChromosome)
+        vChromosome = Chromosome(self)
+        vChromosome.createDefineChromosome(['T','e','s','x','x'])
+        self.chromosome.append(vChromosome)
+        vChromosome = Chromosome(self)
+        vChromosome.createDefineChromosome(['T','e','x','x','x'])
+        self.chromosome.append(vChromosome)
+        vChromosome = Chromosome(self)
+        vChromosome.createDefineChromosome(['T','x','x','x','x'])
       else:
         vChromosome.createRandomChromosome()
 
       self.chromosome.append(vChromosome)
+
 
     pass
 
 
   def getChromosome(self) -> list:
     return self.chromosome
+
+p = Population(3)
+# p.getChromosome()
+
+for i in p.getChromosome():
+  print(i.getGenes())
+
+
+# chromosome = Chromosome('ana',10)
+
+# chromosome.createRandomChromosome()
+
+# print(chromosome.getChromosome())
+# print(chromosome.getFitness())
+
+# chromosome.createDefineChromosome(['a','n','a'])
+
+# print(chromosome.getChromosome())
+# print(chromosome.getFitness())
